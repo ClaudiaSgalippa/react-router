8ebf370639f89cd38c204e9a6df4d2e88fd475ad
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-06 17:27:18 +0200

    Prova di mappatura non riuscita (da rivedere successivamente)

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -2,6 +2,12 @@
 
 import {Link, NavLink} from "react-router-dom"; {/*Import dei componenti di navigazione per routing (tramite libreria React)*/}
 
+{/*const links = [
+    {path:"/", label:"Homepage"},
+    {path:"/chisiamo", label:"ChiSiamo"},
+    {path:"/prodotti", label:"Prodotti"}
+] PROVA DI MAPPATURA - DA CONTROLLARE, error: hook.js:608 Each child in a list should have a unique "key" prop.*/}
+
 const MyNavbar = () => {
     return (
         <nav>
