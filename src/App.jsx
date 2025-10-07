19a367c3607f59e5bb2b2822c33607d9b9e5f95d
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-07 23:38:10 +0200

    Aggiunta "PageNotFound" - Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css' 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'; {/*Import della libreria di ReactRouter*/}
-import Homepage from "./pages/Homepage"; {/*Import della pagina di riferimentor*/}
-import ChiSiamo from "./pages/ChiSiamo"; {/*Import della pagina di riferimentor*/}
-import Prodotti from "./pages/Prodotti"; {/*Import della pagina di riferimentor*/}
+import Homepage from "./pages/Homepage"; {/*Import della pagina di riferimento*/}
+import ChiSiamo from "./pages/ChiSiamo"; {/*Import della pagina di riferimento*/}
+import Prodotti from "./pages/Prodotti"; {/*Import della pagina di riferimento*/}
 import DefaultLayout from './layout/Defaultlayout'; {/*Import del layout di default creato in DefaultLayout*/}
-import DettaglioProdotto from './pages/DettaglioProdotto'; 
+import DettaglioProdotto from "./pages/DettaglioProdotto"; {/*Import della pagina di riferimento*/}
+import PageNotFound from "./pages/PageNotFound"; {/*Import della pagina di riferimento*/}
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
               <Route index element={<Prodotti/>}/> {/*Inserimento della nuova sottorotta figlia*/}
               <Route path=":id" element={<DettaglioProdotto/>}/> {/*Inserimento della sottorotta dinamica con raggruppamento*/}
             </Route> 
+            <Route path="*" element={<PageNotFound/>}/> {/*Tutto quello che non è previsto nelle route precedenti, reinderizzi su una pagina "PageNotFound" - Bonus*/}
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+export default function PageNotFound() {
+    return (
+        <>
+            <div className="patrizia">
+                <h1>Mi dispiace fratellì, stai a cercà nel posto sbagliato...</h1>
+            </div>
+        </>
+    )
+}
