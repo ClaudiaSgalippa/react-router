f553f1c8f57ad5ff3b858743185db7840a425a69
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-07 19:26:45 +0200

    Pre-impostazione mappatura dinamica

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'; {/*Import della l
 import Homepage from "./pages/Homepage"; {/*Import della pagina di riferimentor*/}
 import ChiSiamo from "./pages/ChiSiamo"; {/*Import della pagina di riferimentor*/}
 import Prodotti from "./pages/Prodotti"; {/*Import della pagina di riferimentor*/}
-import DefaultLayout from './layout/Defaultlayout'; {/*Import del layout di default creato in DefaulLayout*/}
+import DefaultLayout from './layout/Defaultlayout'; {/*Import del layout di default creato in DefaultLayout*/}
+import DettaglioProdotto from './pages/DettaglioProdotto'; 
 
 function App() {
 
@@ -12,9 +13,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout/>}> {/*Inserimento della rotta genitore*/}
-            <Route path="/" element={<Homepage/>}/> {/*Inserimento della rotta figlia*/}
-            <Route path="/chisiamo" element={<ChiSiamo/>}/> {/*Inserimento della rotta figlia*/}
-            <Route path="/prodotti" element={<Prodotti/>}/> {/*Inserimento della rotta figlia*/}
+            <Route path="/" element={<Homepage/>}/> {/*Inserimento delle rotte figlie*/}
+            <Route path="/chisiamo" element={<ChiSiamo/>}/>
+            <Route path="/prodotti"> {/*Inserimento della nuova rotta padre*/}
+              <Route index element={<Prodotti/>}/> {/*Inserimento della nuova sottorotta figlia*/}
+              <Route path="/:id" element={<DettaglioProdotto/>}/> {/*Inserimento della sottorotta dinamica con raggruppamento*/}
+            </Route> 
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/DettaglioProdotto.jsx
@@ -0,0 +1,7 @@
+import {useEffect} from "react"; {/*Import dell'hook useEffect*/}
+import {useParams} from "react-router-dom"; {/*Import dell'hook useParams*/}
+
+export default function DettaglioProdotto () { {/*Funzione per l'estrapolazione dei dettagli del singolo prodotto*/}
+    const {id} = useParams(); {/*Estrapolazione dell'id tramite useParams*/}
+
+}
