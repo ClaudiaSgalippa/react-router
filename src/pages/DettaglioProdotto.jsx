ea986bb9975a78af893347691c04c19d5ada2df3
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-07 23:27:42 +0200

    Return della card (con modifiche CSS ) + bonus

--- src/pages/DettaglioProdotto.jsx
+++ src/pages/DettaglioProdotto.jsx
@@ -1,20 +1,52 @@
 import {useState, useEffect} from "react"; {/*Import degli hook useState e useEffect*/}
 import {useParams} from "react-router-dom"; {/*Import dell'hook useParams*/}
+import {Link} from "react-router-dom"; {/*Import dei componenti di navigazione per routing (tramite libreria React)*/}
 
 export default function DettaglioProdotto () { {/*Funzione per l'estrapolazione dei dettagli del singolo prodotto*/}
     const {id} = useParams(); {/*Recupero del parametro "dinamico" aka estrapolazione dell'id tramite useParams*/}
-    const [product, setProduct] = useState ()
+    const [product, setProduct] = useState ();
     
-    useEffect (() => {
+    useEffect (() => { {/*Chiamata API per il recupero dei componenti necessari*/}
         axios.get(`https://fakestoreapi.com/products/${id}`)
-        .then (res => console.log(res.data))
+        .then (res => setProduct(res.data))
         .catch (error => console.log(error))
 
     }, [])
-
-    return (
+                {/*Poniamo il quesito: se esiste questa proprietà, allora richiami la card. Se non esiste, "pagina" di loading*/}
+    return ( 
         <>
-            
+            {product ? 
+                (
+                    <>
+                        <div className="boxCard">
+                            <div className="productCard">
+                                <div className="productImg">
+                                    <img src={product.image} alt={product.title}/>
+                                </div>
+                                <div className="productInfo">
+                                    <p className="productTitle">{product.title}</p>
+                                    <p className="productPrice">{product.price}€</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <Link to={`/prodotti`}>
+                                <h3>Non ti piace? Vabbè, cerca altro va'...</h3>
+                            </Link>
+                        </div>
+                    </>
+                    
+                ) 
+                : 
+                (
+                    <div className="patrizia">
+                        <h2>Aspettate eh, che Patrizia lo sta cercando in magazzino...</h2>
+                        <Link to={`/prodotti`}>
+                            <h3>Se non ti va di aspettare... Torna più tardi!</h3>
+                        </Link>
+                    </div>
+                )
+            }   {/*Bonus caricamento del dettaglio prodotto + navigazione programmatica per il return alla pagina "Prodotti"*/}
         </>
     )
 }
