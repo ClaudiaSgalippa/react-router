94c3b2869ca140db7c5726c5c54efdb2e5178824
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2026-05-06 19:09:13 +0200

    Aggiunta favicon

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
         <>
             <div className="divPages">
                 <h1>BENVENUTI</h1>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1fJth8BK3a8afIQL_tI0HQw5njG7_BvWVJA&s" alt="Biancheria murciano"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1fJth8BK3a8afIQL_tI0HQw5njG7_BvWVJA&s" alt="Biancheria Murciano"/>
             </div>
         </>
     )
