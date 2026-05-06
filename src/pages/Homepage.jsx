ab0691b4e88581198503c13878bd3a76f7b39785
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2026-05-06 19:03:26 +0200

    Piccole modifiche homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
         <>
             <div className="divPages">
                 <h1>BENVENUTI</h1>
-                <img src="https://scontent.ffco5-2.fna.fbcdn.net/v/t39.30808-6/308335385_5414954831922922_6846972892010221731_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Rq2cLE1cwmAQ7kNvwFmjWrE&_nc_oc=Adkn3NCYy94S3znn0u0yFoQ-PD9dPGCXfv3M0v0pygLVOxe7yzQ9kRVuoFeuU2mIeavGbOIemjdRMZs6i7zgEtR8&_nc_zt=23&_nc_ht=scontent.ffco5-2.fna&_nc_gid=nYhEAtxMqaE9RTriG2DsRw&oh=00_AfctO4OuUqaNDLNWl3qnLsibsJxzL9bXOgPefzqXhKDtjQ&oe=68E9F3F5" alt="Biancheria murciano"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1fJth8BK3a8afIQL_tI0HQw5njG7_BvWVJA&s" alt="Biancheria murciano"/>
             </div>
         </>
     )
